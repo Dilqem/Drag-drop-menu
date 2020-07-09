18b3127c0835c76938cc3161455386b56c391a54
Dilqem <44258938+Dilqem@users.noreply.github.com>
2020-07-09 05:46:31 +0400

    Add files via upload

---
+++ drag-drop-html/index.js
@@ -0,0 +1,11 @@
+function allowdrop(ev){
+    	ev.preventDefault();
+    }
+function pull(ev){
+      ev.dataTransfer.setData("text", ev.target.id);
+    }
+function drop(ev){
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+    }
